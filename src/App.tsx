6e80dcdc9e7686d8f08a8d1d30ee8c53257d0757
catnipan <yifan@catnipan.com>
2024-10-19 23:05:16 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./tailwind.css";
+
+const App = () => {
+  return (
+    <div className="bg-neutral-800 h-screen flex justify-center items-stretch text-center flex-col text-neutral-600 gap-5 selection:bg-neutral-950">
+      <h1 className="text-6xl uppercase font-bold">maze rendezvous</h1>
+      <h2>- working in progress -</h2>
+    </div>
+  );
+};
+
+export default App;
